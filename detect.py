b090a1c8da590648e1e4bef4f8f015eb11dc6989
MichailNikitin <82164551+MichailNikitin@users.noreply.github.com>
2022-04-20 16:47:32 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -6,6 +6,14 @@ import sys
 from pathlib import Path
 
 import cv2
+import torch# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
@@ -22,25 +30,21 @@ from utils.general import (LOGGER, check_file, check_img_size, check_imshow, che
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 
-conf_thres = 0.60  # confidence threshold
-iou_thres = 0.80  # NMS IOU threshold
-max_det = 10
-
 
 @torch.no_grad()
-def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
-        source= ROOT / "0",  # file/dir/URL/glob, 0 for webcam
+def inicial(weights='barhuman_yolov5.pt',  # model.pt path(s)
+        source=ROOT / "0",  # file/dir/URL/glob, 0 for webcam
         data=r'\data\crowdhuman.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=conf_thres,  # confidence threshold
-        iou_thres=iou_thres,  # NMS IOU threshold
-        max_det=max_det,  # maximum detections per image
+        conf_thres=0.60,  # confidence threshold
+        iou_thres=0.80,  # NMS IOU threshold
+        max_det=1,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
         save_txt=True,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
+        save_conf=True,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
-        nosave=True,  # do not save images/videos
+        nosave=False,  # do not save images/videos
         classes=3,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
@@ -55,6 +59,55 @@ def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
         ):
+
+    global model, stride, names, pt, jit, onnx, engine,devices
+
+    # Load model
+    devices = select_device(device)
+    model = DetectMultiBackend(weights, device=devices, dnn=dnn, data=data)
+    stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Half
+    if pt or jit:
+        model.model.half() if half else model.model.float()
+
+
+@torch.no_grad()
+def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
+        source=ROOT / "0",  # file/dir/URL/glob, 0 for webcam
+        data=r'\data\crowdhuman.yaml',  # dataset.yaml path
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.60,  # confidence threshold
+        iou_thres=0.80,  # NMS IOU threshold
+        max_det=1,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
+        save_txt=True,  # save results to *.txt
+        save_conf=True,  # save confidences in --save-txt labels
+        save_crop=False,  # save cropped prediction boxes
+        nosave=False,  # do not save images/videos
+        classes=3,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project=ROOT / 'runs',  # save results to project/name
+        name='test',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=20,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        ):
+
+    save_txt = True
+    save_conf = True
+    view_img = True
+    max_det = 1
+    global obj_x
+
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
@@ -67,35 +120,24 @@ def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
     save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
-    # Load model
-    device = select_device(device)
-    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data)
-    stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
-
-    # Half
-    half &= (pt or jit or onnx or engine) and device.type != 'cpu'  # FP16 supported on limited backends with CUDA
-    if pt or jit:
-        model.model.half() if half else model.model.float()
 
     # Dataloader
     if webcam:
-        view_img = check_imshow()
+        # view_img = check_imshow()
         cudnn.benchmark = True  # set True to speed up constant image size inference
         dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
         bs = len(dataset)  # batch_size
     else:
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
         bs = 1  # batch_size
-    vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
-    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
     for path, im, im0s, vid_cap, s in dataset:
         t1 = time_sync()
-        im = torch.from_numpy(im).to(device)
-        im = im.half() if half else im.float()  # uint8 to fp16/32
+        im = torch.from_numpy(im).to(devices)
+        im = im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
@@ -112,7 +154,7 @@ def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
         dt[2] += time_sync() - t3
 
-        # Second-stage classifier (optional)
+        # Second-stage classifier
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
 
         # Process predictions
@@ -142,11 +184,15 @@ def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    obj_x = xywh[0]
                     if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
+
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                            f.write(('%g ' * len(
+                                line)).rstrip() % line + " " + f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
@@ -157,39 +203,20 @@ def run(weights='barhuman_yolov5.pt',  # model.pt path(s)
 
             # Stream results
             im0 = annotator.result()
-            '''
+
             if view_img:
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
-            '''
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer[i].write(im0)
 
-        # Print time (inference-only)
-        LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+        # Print results
+        if abs(obj_x - 0.5) < 0.05:
+            LOGGER.info("center")
+        elif obj_x > 0.5:
+            LOGGER.info("right")
+        else:
+            LOGGER.info("left")
+        #LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
 
-    # Print results
-    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
 
@@ -199,10 +226,11 @@ def parse_opt():
     parser.add_argument('--weights', nargs='+', type=str, default='barhuman_yolov5.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=0, help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=f'data\crowdhuman.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[128], help='inference size h,w')  #640
-    parser.add_argument('--conf-thres', type=float, default=conf_thres, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=iou_thres, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=max_det, help='maximum detections per image')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[128],
+                        help='inference size h,w')  # 640
+    parser.add_argument('--conf-thres', type=float, default=0.60, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.80, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=2, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='show results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
@@ -230,8 +258,8 @@ def parse_opt():
 
 def main(opt):
     check_requirements(exclude=('tensorboard', 'thop'))
+    inicial(**vars(opt))
     run(**vars(opt))
-
 
 if __name__ == "__main__":
     opt = parse_opt()
